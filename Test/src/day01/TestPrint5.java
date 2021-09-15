d6fc7d02abc7f95e60ce7502a7aec75bfb93337c
gaojian <gaojian_lisp@outlook.com>
2021-09-15 21:10:44 +0800

    今日java练习题

---
+++ Test/src/day01/TestPrint5.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class TestPrint5 {
+    public static void main(String[] args) {
+        int red =25;
+        red = red*2+5;
+        int blue =30;
+        blue=blue*2;
+        System.out.println(red ==blue);
+    }
+}
