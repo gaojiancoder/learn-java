89c53d9ed890fb4cd0516984983ddfea5e75d231
gaojian <gaojian_lisp@outlook.com>
2021-09-17 21:25:33 +0800

    今日java练习题

---
+++ Test/src/day01/TestPrint9.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class TestPrint9 {
+    public static void main(String[] args) {
+        int plan1=8888-1880;
+        double plan2 =8888*0.75;
+        if (plan1>plan2){
+            System.out.println("使用以旧换新更省钱");
+        }else {
+            System.out.println("不使用以旧换新更省钱");
+        }
+    }
+}
