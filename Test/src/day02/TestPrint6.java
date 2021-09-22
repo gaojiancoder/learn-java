2edb3890f20258aa138bbe2b92c53b6d785fa0a8
gaojian <gaojian_lisp@outlook.com>
2021-09-22 21:53:35 +0800

    今日java练习题

---
+++ Test/src/day02/TestPrint6.java
@@ -0,0 +1,19 @@
+package day02;
+
+
+public class TestPrint6 {
+    public static void main(String[] args) {
+        //for循环的死循环
+//        for (int i = 1; ; i++){
+//            System.out.println("欢迎学习泡泡的分享,继续加油哦~");
+//        }
+        //while循环的死循环
+//        while(true){
+//            System.out.println("都到这里啦?奥利给~");
+//        }
+        //do-while循环的死循环
+        do{
+            System.out.println("相信我们可以的~");
+        }while(true);
+    }
+}
